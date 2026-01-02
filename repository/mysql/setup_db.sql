fbbab611c9a2f860a82a3b5c4abb94bd886d3f0c
Sahar Mokarrami <sahar.mokarrami94@gmail.com>
2026-01-02 19:50:00 +0330

    feature: add mysql database dockerfile

---
+++ repository/mysql/setup_db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    id int not null ,
+    name varchar(100),
+    phone varchar(15)
+);
---
+++ repository/mysql/user.go
@@ -0,0 +1,11 @@
+package mysql
+
+import "gameapp/entity"
+
+func (d DB) IsPhoneNumberUnique(phonenumber string) (bool, error) {
+
+}
+
+func (d DB) Register(u entity.User) (entity.User, error) {
+
+}
