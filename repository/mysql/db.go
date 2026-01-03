2a28c1ed1153848c95353fd9a1c1deeeba6f34fe
Sahar Mokarrami <sahar.mokarrami94@gmail.com>
2026-01-03 15:44:43 +0330

    chore: add database mysql connection

--- repository/mysql/db.go
+++ repository/mysql/db.go
@@ -1,4 +1,25 @@
 package mysql
 
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+)
+
 type DB struct {
+	db *sql.DB
+}
+
+func New() *DB {
+	db, err := sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		panic(fmt.Errorf("can not connect to database: %v", err))
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return &DB{db}
 }
--- repository/mysql/setup_db.sql
+++ repository/mysql/setup_db.sql
@@ -1,5 +1,7 @@
 CREATE TABLE users (
-    id int not null ,
-    name varchar(100),
-    phone varchar(15)
+    id int primary key AUTO_INCREMENT,
+    name varchar(100) not null,
+    phone varchar(15) not null unique,
+    created_at timestamp CURRENT_TIMESTAMP ,
+    updated_at timestamp CURRENT_TIMESTAMP
 );
